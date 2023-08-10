41df9c82ea8c1dbaf4069732aae5cb028501b2cf
Iqbal Fauzi <iqbalfawz@gmail.com>
2023-08-10 17:31:31 +0700

    Change setting group

--- modules/site-contact/migrate.php
+++ modules/site-contact/migrate.php
@@ -7,13 +7,13 @@ return [
                 [
                     'name' => 'site_contact_title',
                     'type' => 1,
-                    'group' => 'Site Contact',
+                    'group' => 'Contact',
                     'value' => 'Contact us'
                 ],
                 [
                     'name' => 'site_contact_description',
                     'type' => 1,
-                    'group' => 'Site Contact',
+                    'group' => 'Contact',
                     'value' => 'Please fill form below and hit send button to contact our support'
                 ]
             ]
